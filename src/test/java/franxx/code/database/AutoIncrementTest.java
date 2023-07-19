ca57128cabed318b76a699bc477c4a3949e5efa2
reimiii <imiia75775@gmail.com>
2023-07-19 19:12:11 +0700

    AutoIncrementTest

---
+++ src/test/java/franxx/code/database/AutoIncrementTest.java
@@ -0,0 +1,35 @@
+package franxx.code.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+public class AutoIncrementTest {
+    @Test
+    void testAutoIncrement() throws SQLException {
+        Connection connection = ConnectionUtil.getSource().getConnection();
+
+        String sql = """
+                insert into comments (email, comment) values (?, ?);
+                """;
+
+        PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+
+        statement.setString(1, "Ze@Mail.com");
+        statement.setString(2, "WHoOOOAA");
+
+
+        statement.executeUpdate();
+
+        ResultSet keys = statement.getGeneratedKeys();
+
+        if (keys.next()) {
+            System.out.println("Id Comment: " + keys.getInt(1));
+        }
+
+
+        statement.close();
+        connection.close();
+    }
+}
